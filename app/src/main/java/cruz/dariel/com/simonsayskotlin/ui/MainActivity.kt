cee5ed2f0bf79fbdabb6cbf907b504d0e245ba10
Dariel Cruz Hernandez <darielnaturalist@gmail.com>
2020-06-21 22:36:47 -0400

    adding back navigation

--- app/src/main/java/cruz/dariel/com/simonsayskotlin/ui/MainActivity.kt
+++ app/src/main/java/cruz/dariel/com/simonsayskotlin/ui/MainActivity.kt
@@ -3,13 +3,30 @@ package cruz.dariel.com.simonsayskotlin.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import cruz.dariel.com.simonsayskotlin.R
 import cruz.dariel.com.simonsayskotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val myNavHostFragment by lazy {
+        this.findNavController(R.id.myNavHostFragment)
+    }
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        appBarConfiguration = AppBarConfiguration(myNavHostFragment.graph)
+        NavigationUI.setupActionBarWithNavController(this, myNavHostFragment)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(myNavHostFragment, appBarConfiguration)
     }
 }
