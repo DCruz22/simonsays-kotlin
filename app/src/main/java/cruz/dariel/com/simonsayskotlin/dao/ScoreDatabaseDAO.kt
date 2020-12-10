3e6c34943bf23205d1ad3fadc72c4c1fc488a7f6
Dariel Cruz Hernandez <darielnaturalist@gmail.com>
2020-12-10 14:23:37 -0400

    adding views to game layout

--- app/src/main/java/cruz/dariel/com/simonsayskotlin/dao/ScoreDatabaseDAO.kt
+++ app/src/main/java/cruz/dariel/com/simonsayskotlin/dao/ScoreDatabaseDAO.kt
@@ -1,24 +1,26 @@
 package cruz.dariel.com.simonsayskotlin.dao
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import cruz.dariel.com.simonsayskotlin.models.Score
 
+@Dao
 interface ScoreDatabaseDAO {
 
     @Insert
-    fun insert(score: Score)
+    suspend fun insert(score: Score)
 
     @Query("Select * from score_table ORDER BY id DESC")
-    suspend fun getAll() : LiveData<List<Score>>
+    fun getAll() : LiveData<List<Score>>
 
     @Query("SELECT * from score_table WHERE playerName = :name")
-    suspend fun search(name: String) : LiveData<List<Score>>
+    fun search(name: String) : LiveData<List<Score>>
 
     @Query("SELECT * FROM score_table ORDER BY id DESC LIMIT 1")
-    suspend fun getLastScore(): Score?
+    fun getLastScore(): LiveData<Score?>
 
     @Query("SELECT * FROM score_table ORDER BY score DESC LIMIT 1")
-    suspend fun getHighestScore(): LiveData<Score>
+    fun getHighestScore(): LiveData<Score?>
 }
